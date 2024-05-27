d766de6007dced37d920d7cc546d55d66e0c57e4
David <davidllumitaxi67@gmail.com>
2024-05-26 23:03:51 -0500

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react'
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
+import { db } from './data/db'
 
 function App() {
+
+  const [data, setData] = useState(db) //inicializa con la db
+  const [cart, setCart] = useState([])
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+    if(itemExists >= 0){// existe en el carrito
+      console.log('Ya Exite....')
+    } else{
+      //setCart(prevCart => [...prevCart, item]) //Setea al carrito
+      setCart([...cart, item]) 
+    }
+
+  }
 
   return (
     <>
@@ -10,7 +26,16 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
+          {data.map((guitar) => ( //Itera db, lo nombre como guitar
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
+
+
         </div>
 
       </main>
